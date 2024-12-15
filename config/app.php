5726d1bbd3873274e2b3149dc88c02417fa3daa0
Vortech Studio <contact@vortechstudio.fr>
2024-12-15 19:25:32 +0100

    feat(updater): ajout de la fonctionnalité de vérification et de mise à jour (main)

--- config/app.php
+++ config/app.php
@@ -2,10 +2,12 @@
 return [
     "app_key" => "base64:kHxAxRhJJ5GkiL0o1vndLMQ1AH/cL6nR0k+Gcga+2ko=",
     "name"=> "Mod Manager",
-    "version"=> "1.5.0",
+    "version"=> "1.5.1",
     "env"=> "development",
     'key' => 'base64:kHxAxRhJJ5GkiL0o1vndLMQ1AH/cL6nR0k+Gcga+2ko=',
     'cipher' => 'AES-256-CBC',
+    'github_owner' => "vortechstudio",
+    'github_repo' => "Mod-Manager",
     "providers"=> [
         App\Providers\AppServiceProvider::class,
     ],
