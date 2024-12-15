ff75b8d188e146d1ba93c0093a1fb92859b9e5ad
Vortech Studio <contact@vortechstudio.fr>
2024-12-15 22:20:29 +0100

    fix: mettre à jour la version de l'application à 1.6.0 (main)

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 return [
     "app_key" => "base64:kHxAxRhJJ5GkiL0o1vndLMQ1AH/cL6nR0k+Gcga+2ko=",
     "name"=> "Mod Manager",
-    "version"=> "1.5.1",
+    "version"=> "1.6.0",
     "env"=> "development",
     'key' => 'base64:kHxAxRhJJ5GkiL0o1vndLMQ1AH/cL6nR0k+Gcga+2ko=',
     'cipher' => 'AES-256-CBC',
